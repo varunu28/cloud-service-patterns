2aa82589cfc5aca839ce8c677395d44d897c3993
varunu28 <varun.u28@gmail.com>
2025-03-08 09:34:10 -0800

    Kubernetes ConfigMap working. Next config refresh

--- deployment/build_image.sh
+++ deployment/build_image.sh
@@ -11,3 +11,13 @@ cd ../webapp
 ./mvnw spring-boot:build-image
 
 cd ../deployment
+
+# Remove existing images from minikube
+minikube image rm varunu2892/greeting-service:latest
+minikube image rm varunu2892/name-service:latest
+minikube image rm varunu2892/webapp:latest
+
+# Load images into minikube
+minikube image load varunu2892/greeting-service:latest
+minikube image load varunu2892/name-service:latest
+minikube image load varunu2892/webapp:latest
