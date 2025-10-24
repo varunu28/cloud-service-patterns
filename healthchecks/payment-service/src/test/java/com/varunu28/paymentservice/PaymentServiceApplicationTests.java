08bdf18b78660c0190d4a038963d4914ca7bee99
varunu28 <varun.u28@gmail.com>
2025-10-24 06:09:35 -0700

    Add placeholder services for healthchecks

---
+++ healthchecks/payment-service/src/test/java/com/varunu28/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.varunu28.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
