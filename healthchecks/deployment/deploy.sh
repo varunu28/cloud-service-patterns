8aa1af1d2473f5e06794d3ff0f5060ff87c90f40
varunu28 <varun.u28@gmail.com>
2025-11-01 13:02:28 -0700

    Services successfully deployed to K8S. Probes next

--- healthchecks/deployment/deploy.sh
+++ healthchecks/deployment/deploy.sh
@@ -9,16 +9,25 @@ cd infra
 kubectl apply -f paymentdb-postgres-secret.yml
 kubectl apply -f paymentdb-postgres-configmap.yml
 kubectl apply -f paymentdb-postgres-deployment.yml
+kubectl apply -f order-service-secret.yml
+kubectl apply -f orderdb-postgres-configmap.yml
+kubectl apply -f orderdb-postgres-deployment.yml
+kubectl apply -f order-service-rabbitmq-deployment.yml
 
 ## Build service images
 cd ../../payment-service
 ./mvnw spring-boot:build-image
+cd ../order-service
+./mvnw spring-boot:build-image
 
+## Return to root directory
 cd ..
 
 ## Upload service images to minikube
 minikube image load varunu2892/payment-service:latest
+minikube image load varunu2892/order-service:latest
 
 # Deploy services
 cd deployment
 kubectl apply -f services/payment-service.yml
+kubectl apply -f services/order-service.yml
