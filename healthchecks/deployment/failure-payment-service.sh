6d53b8dc42b443af1733224aed6607fa91fc481a
varunu28 <varun.u28@gmail.com>
2025-11-02 07:26:03 -0800

    Payment service failure injection

---
+++ healthchecks/deployment/failure-payment-service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Build only the payment service image with simulated failure
+cd ../payment-service
+./mvnw spring-boot:build-image
+
+## Return to root directory
+cd ..
+
+## Upload payment service image to minikube
+minikube image load varunu2892/payment-service:latest
+
+# Deploy payment service
+cd deployment
+kubectl apply -f services/payment-service.yml
