3025db0c4e4e49637946740f1e296a5eb40378d6
varunu28 <varun.u28@gmail.com>
2026-03-27 06:07:31 -0700

    Initial set of services for poll based outbox

---
+++ outbox/poll-based/notificationservice/src/test/java/com/varunu28/notificationservice/NotificationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varunu28.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
