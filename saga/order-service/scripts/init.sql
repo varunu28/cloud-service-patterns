38374d2c4b16b119fe8593200997766d9ad17ac3
varunu28 <varun.u28@gmail.com>
2026-05-15 18:08:15 +0530

    Add schema for order events

--- saga/order-service/scripts/init.sql
+++ saga/order-service/scripts/init.sql
@@ -7,3 +7,13 @@ CREATE TABLE IF NOT EXISTS orders (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE IF NOT EXISTS order_events (
+    id BIGSERIAL PRIMARY KEY,
+    order_id BIGINT NOT NULL,
+    event_name VARCHAR(255) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    CONSTRAINT fk_order FOREIGN KEY(order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    CONSTRAINT ue_order_id_event_name UNIQUE (order_id, event_name)
+);
--- saga/payment-service/src/main/kotlin/com/saga/paymentservice/kafka/KafkaTopics.kt
+++ saga/payment-service/src/main/kotlin/com/saga/paymentservice/kafka/KafkaTopics.kt
@@ -4,6 +4,4 @@ object KafkaTopics {
     const val ORDER_CREATED_TOPIC = "order-created"
     const val PAYMENT_SUCCEEDED_TOPIC = "payment-succeeded"
     const val PAYMENT_FAILED_TOPIC = "payment-failed"
-    const val INVENTORY_RESERVATION_SUCCESSFUL_TOPIC = "inventory-reservation-successful"
-    const val INVENTORY_RESERVATION_FAILED_TOPIC = "inventory-reservation-failed"
 }
